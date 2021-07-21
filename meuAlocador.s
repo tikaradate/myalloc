e5e829ab11c3e18627b8f6fd27a5c50cff17cd96
Jorge Lucas V Jabczenski <40120369+JorgeJabczenski@users.noreply.github.com>
2021-07-21 18:53:55 -0300

    alocaMem burro feito

--- meuAlocador.s
+++ meuAlocador.s
@@ -5,13 +5,89 @@ str:    .string "################"
 
 .section .text
 
-# alocaMem liberaMem imprimeMapa
-; exemplo:
-;     pushq %rbp          # manda pra pilha rbp
-;     movq %rsp, %rbp     # rbp aponta pra rsp
+.globl alocaMem
+alocaMem:
 
-;     popq %rbp
-;     ret
+    pushq %rbp
+    movq  %rsp, %rbp
+
+    mov %rdi           , tamAloc # salva o tamanho da alocacao
+    mov topoInicialHeap, %r10    # Salva o topo da heap em r10
+
+    mov  $12, %rax               # codigo referente ao brk
+    mov  $0 , %rdi               # 0 para retornar o topo da heap
+    syscall                      # executa a sycall
+    movq %rax, topoAtualHeap     # retorno em %rax e salvo em topoAtualHeap
+
+    w0: 
+        cmpq %r10 , topoAtualHeap         # compara o topo inicial (r10) com o topo atual (rax)
+        je fim_w0
+
+        mov (%r10), %r15                  # bloco de "livre ou não"  -> r15
+        cmp $0    , %r15                  # se ocupado, procura proximo bloco
+        jne prox_bloc_1
+
+        mov %r10     , %r14               # compara se o tamanho disponível é o suficiente para alocar 
+        add $8       , %r10               # o novo tamanho
+        mov (%r10)   , %r15 
+        cmpq tamAloc , %r15
+        jl  prox_bloc_2
+
+        mov $1      ,  %r13               # se nao está ocupado e tem tamanho o suficiente 
+        mov %r13    , (%r14)
+        movq tamAloc,  %r14
+        mov %r14    , (%r15)
+
+        add $8      , %r15 
+
+        mov %r15    , %rax 
+        
+
+        jmp fim_aloc             # Termina a alocação
+
+        prox_bloc_1:
+        add $8       , %r10
+        mov (%r10)   , %r15
+
+        prox_bloc_2:
+        add $8       , %r10
+
+        add %r15     , %r10
+
+        jmp w0
+
+    fim_w0:
+    # Não tinha um espaço disponivel, é necessário alocar um novo
+
+    mov  $12, %rax               # codigo referente ao brk
+    mov  $0 , %rdi               # 0 para retornar o topo da heap
+    syscall                      # executa a sycall
+
+    mov %rax    , %r15           # salva o local atual
+    
+   
+    addq tamAloc, %rax           # soma o tamanho da alocação
+    add  $16    , %rax           # soma o espaço do cabecalho
+
+    mov %rax    , %rdi           # move o tamanho para a chamado do brk
+    mov  $12    , %rax           # codigo referente ao brk
+    syscall                      # executa a sycall
+
+    mov $1       ,  %r14         # a[0] espaco alocado
+    mov %r14     , (%r15)        #
+    add $8       ,  %r15         # a[1]
+    movq tamAloc ,  %r14         # 
+    mov %r14     , (%r15)        # a[1] = tamanho
+    mov %r15     , %rax
+    add $8       , %rax
+
+    fim_aloc:
+
+    popq %rbp
+
+ret
+
+
 .globl iniciaAlocador 
 iniciaAlocador:
     pushq %rbp          # manda pra pilha rbp
